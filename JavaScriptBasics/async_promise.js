c42c7cdb9a0fca6c346981eb8abc38f45e8d84f4
Zach.z.li.hp7_win <zhuo.li.ca@outlook.com>
2022-04-12 16:18:19 +0800

    refresh done

--- JavaScriptBasics/async_promise.js
+++ JavaScriptBasics/async_promise.js
@@ -1,4 +1,7 @@
 const fetchData = (callback) => {
+    const promise = new Promise((resolve, reject) => {
+        
+    });
     setTimeout(
         () => {
             callback('Done!');
@@ -8,14 +11,39 @@ const fetchData = (callback) => {
 
 
 
+const fetchData1 = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!');
+        }, 1500); 
+    });
+    return promise;
+}
+
+
 // async code
 setTimeout(
     () => {
-        console.log('Timer is done!')
+        console.log('Timer is done!');
+        fetchData(text => {
+            console.log(text);
+        })
     },
-    1
+    2000
 ); // callback function
 
+
+setTimeout(() => {
+    console.log('Timer is done!');
+    fetchData1().then(
+        text => {
+            console.log(text);
+            return fetchData1();
+        }
+    ).then(text2 => {
+        console.log(text2);
+    })
+})
 
 
 // sync code
