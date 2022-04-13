2a63b5d1312addb91f594c04bf23869a0cda17e0
Zach.z.li.hp7_win <zhuo.li.ca@outlook.com>
2022-04-13 21:23:09 +0800

    express.js basics

---
+++ 05_Working_with_expressjs/routes/admin.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+const rootDir = require('../util/path');
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
+
+
+// /admin/add-product => POST
+router.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = router;
