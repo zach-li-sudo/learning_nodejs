5aedba686b3698f2cd419dcd8f91a45b826da905
Zach.z.li.hp7_win <zhuo.li.ca@outlook.com>
2022-04-17 15:22:15 +0800

    mvc

--- 07_Model_View_Controller_MVC/app.js
+++ 07_Model_View_Controller_MVC/app.js
@@ -8,17 +8,22 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+
+// console.log(adminRoutes);
+
+app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+
+const errorController = require('./controllers/error');
+
+// app.use(errorController.get404);
+app.use(errorController.get404);
 
 app.listen(3000);
