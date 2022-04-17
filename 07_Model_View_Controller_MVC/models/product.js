4e8cb537c9ffad95e02048c311d7ac1e106453bf
Zach.z.li.hp7_win <zhuo.li.ca@outlook.com>
2022-04-17 15:51:25 +0800

    refactorize file code

--- 07_Model_View_Controller_MVC/models/product.js
+++ 07_Model_View_Controller_MVC/models/product.js
@@ -1,4 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const rootDir = path.dirname(process.mainModule.filename);
+
 const products = [];
+const p = path.join(rootDir, 'data', 'products.json');
+const getProductsFromFile = (cb) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    }); 
+    return products;
+}
+
+
 
 module.exports = class Product {
     constructor(title) {
@@ -6,11 +22,16 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err);
+            })
+        });
     }
 
     // called by class Prototype, not instances
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
     }
 }
