059183230ec811972b1068ca90048660332b2969
Zach Li <58021074+zach-li-sudo@users.noreply.github.com>
2022-04-25 12:37:51 +0800

    init

--- 12_MongoDB/util/database.js
+++ 12_MongoDB/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'hola', {
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
   dialect: 'mysql',
   host: 'localhost'
 });
