7bf4363b63d90c332a1790480ad901b5cd13a94b
Zach Li <58021074+zach-li-sudo@users.noreply.github.com>
2022-04-26 15:37:53 +0800

    jwt test

--- 30_testing_js/test/auth-middleware.js
+++ 30_testing_js/test/auth-middleware.js
@@ -2,13 +2,35 @@ const expect = require('chai').expect;
 
 const authMiddleware =  require('../middleware/is-auth');
 
-it('should throw an error if no authorization header is present', function() {
-    const req = {
-        get: function(headerName) {
-            return null;
-        }
-    };
+describe('Auth middleware', function() {    
+    it('should throw an error if no authorization header is present', function() {
+        const req = {
+            get: function(headerName) {
+                return null;
+            }
+        };
+        
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not authenticated.');
+    });
+    
+    
+    it('should throw an error if the authorization header is only one string', function() {
+        const req = {
+            get: function(headerName) {
+                return 'xyz';
+            }
+        };
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+    });
 
-    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not authenticated.');
-});
+    it('should yield a userId after decoding the token', function() {
+        const req = {
+            get: function(headerName) {
+                return 'Bearer datseastkttsast';
+            }
+        };
+        authMiddleware(req, {}, () => {});
+        expect(req).to.have.property('userId');
+    });
 
+})
